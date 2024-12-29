16356e5a167375e50092b7c435a74ba3d597f546
Sebnem <sebkay2002@hotmail.com>
2024-12-29 22:11:16 +0300

    first

---
+++ src/components/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Description from "./Description/Description";
+import Feedback from "./Feedback/Feedback";
+import Options from "./Options/Options";
+import Notification from "./Notification/Notification";
+
+function App() {
+  const [values, setValues] = useState(() => {
+    const savedValues = window.localStorage.getItem("saved-values");
+
+    if (savedValues !== null) {
+      return JSON.parse(savedValues);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  const totalFeedback = values.good + values.neutral + values.bad;
+
+  function updateFeedbackRef(feedbackType) {
+    if (feedbackType === "reset") {
+      setValues({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      });
+      return;
+    }
+
+    setValues({
+      ...values,
+      [feedbackType]: values[feedbackType] + 1,
+    });
+    return;
+  }
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-values", JSON.stringify(values));
+  }, [values]);
+
+  return (
+    <>
+      <Description />
+      <Options update={updateFeedbackRef} total={totalFeedback} />
+      {totalFeedback > 0 ? <Feedback values={values} /> : <Notification />}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,24 @@
+import c from "./Options.module.css";
+
+const Options = ({ update, total }) => {
+  return (
+    <div className={c.options}>
+      <button className={c.btn} onClick={() => update("good")}>
+        Good
+      </button>
+      <button className={c.btn} onClick={() => update("neutral")}>
+        Neutral
+      </button>
+      <button className={c.btn} onClick={() => update("bad")}>
+        Bad
+      </button>
+      {total > 0 && (
+        <button className={c.btn} onClick={() => update("reset")}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Options;
